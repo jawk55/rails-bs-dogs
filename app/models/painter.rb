d40ce0177e1bfb2e6974d9d329f0ae4d377cad57
dell@dell.com <dell@dell.com>
2022-10-04 18:37:35 -0300

    add changes

--- app/models/painter.rb
+++ app/models/painter.rb
@@ -1,4 +1,10 @@
 class Painter < ApplicationRecord
   has_many :picture, dependent: :destroy
   accepts_nested_attributes_for :picture
+
+  # attr_accessor :name#, :email, :content
+
+  # validates_presence_of :name
+  # validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
+  # validates_length_of :content, :maximum => 500
 end
