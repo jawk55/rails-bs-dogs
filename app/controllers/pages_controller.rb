f06d8e833f20a5765450ec6612f00e88fd7bd198
dell@dell.com <dell@dell.com>
2022-10-05 19:17:10 -0300

    changes in devise text languague and solve minors

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
+  # before_action :authenticate_user!
   def home
     @a = 3
+    flash[:success] = "msg."
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root "pages#home"
+
+
+
   resources :painters do
     resources :pictures
   end
@@ -15,5 +20,4 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pages#home"
 end
