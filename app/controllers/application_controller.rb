93d88a2a127eec19c27f85b49eeabf42722ea747
dell@dell.com <dell@dell.com>
2022-10-06 20:31:27 -0300

    add roles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configured_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configured_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone])
+  end
+
+  def tengo_permisos?
+    if !current_user.admin?
+      redirect_to articles_path, alert: "No tiene permiso"
+    end
+  end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-
+  before_action :tengo_permisos?, except: [:index,:show]
   before_action :set_article, only: %i[ show edit update destroy ]
 
   # GET /articles or /articles.json
