479429a7c0117db67b5787535de8b0614b03de4a
dell@dell.com <dell@dell.com>
2022-10-07 19:56:24 -0300

    erase spaces in models

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,4 @@
 class Client < ApplicationRecord
   has_many :reservations
   has_many :restaurants, through: :reservations
-
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,4 @@
 class Restaurant < ApplicationRecord
   has_many :reservations
   has_many :clients, through: :reservations
-
 end
