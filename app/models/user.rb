cf2a7b217a6f6c3ab6b249b8764d706f67d930fe
dell@dell.com <dell@dell.com>
2022-10-07 20:37:50 -0300

    add bs card to product main view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
-  after_initialize :set_default_role, :if => :new_record?
+  after_initialize :rol_por_defecto, :if => :new_record?
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable
 
-  enum role: [:user, :moderator, :admin]
-  def set_default_role
-    self.role ||= :user
+  # enum role: [:user, :moderator, :admin]
+  enum role: [:vendedor, :admin]
+
+  def rol_por_defecto
+    self.role ||= :vendedor
   end
 
 end
